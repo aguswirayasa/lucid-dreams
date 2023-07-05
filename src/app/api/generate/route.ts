db3af8bc8920479bd0f26a310e780117995220e3
aguswirayasa <aguswirayasa08@gmail.com>
2023-07-05 19:58:08 +0800

    okay fix

--- src/app/api/generate/route.ts
+++ src/app/api/generate/route.ts
@@ -1,8 +1,6 @@
 import axios from "axios";
 import { NextRequest, NextResponse } from "next/server";
-export const config = {
-  runtime: "edge",
-};
+export const runtime = "edge";
 export async function POST(request: NextRequest) {
   const { prompt, negativePrompt } = await request.json();
 
