731cb9828426fde9dcfcabc2eeafccd41d5113fb
aguswirayasa <aguswirayasa08@gmail.com>
2023-07-05 19:56:11 +0800

    fix

--- src/app/api/post/route.ts
+++ src/app/api/post/route.ts
@@ -2,9 +2,7 @@ import { NextRequest, NextResponse } from "next/server";
 
 import { firestore } from "../../../../firebase/firebase";
 import { cloudinary } from "../../../../cloudinary.config";
-export const config = {
-  runtime: "edge",
-};
+
 export async function POST(request: NextRequest) {
   const { postData } = await request.json();
   const { imageUrl, username, prompt, negativePrompt, model } = postData;
