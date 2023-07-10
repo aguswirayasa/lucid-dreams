c5fd51cae3e95d962749ddd51841608fa7358be3
aguswirayasa <aguswirayasa08@gmail.com>
2023-07-11 01:08:36 +0800

    fixing api and add random and model

---
+++ data/index.ts
@@ -0,0 +1,85 @@
+export const ApiModels = [
+  {
+    id: "fuwafuwamix",
+    name: "FuwaFuwaMix",
+  },
+  {
+    id: "Anything V5",
+    name: "anything-v5",
+  },
+  {
+    id: "Anything V4",
+    name: "anything-v4",
+  },
+  {
+    id: "Anything V3",
+    name: "anything-v3",
+  },
+  {
+    id: "dark-sushi-mix",
+    name: "Dark Sushi Mix",
+  },
+  {
+    id: "counterfeit-v30",
+    name: "Counterfeit-v3.0",
+  },
+  {
+    id: "abyssorangemix2nsfw",
+    name: "AbyssOrangeMix2",
+  },
+];
+
+export const randoms = [
+  {
+    prompt:
+      "2 girl,gyaru,looking at viewer,maid outfit,big breast,cat ears,white hair,red eyes,perfect face,naughty smiles,hug,bedroom,masterpiece,high quality",
+    negative:
+      "(bad quality,low quality :1.6), multiple body part,incomplete body part,merged body part,deformed,blurry,bad anatomy,disfigured,poorly drawn face,mutation,extra_limb,ugly,poorly drawn hands,two heads,gross,disgusting,horrible",
+  },
+  {
+    prompt:
+      "looking at viewer,gyaru,mold,white hair,red eye,smirk,crop t-shirt,white hats,choker,naval piercing,crowded street",
+    negative:
+      "(bad quality,low quality :1.6), multiple body part,incomplete body part,merged body part,deformed,blurry,bad anatomy,disfigured,poorly drawn face,mutation,extra_limb,ugly,poorly drawn hands,two heads,gross,disgusting,horrible",
+  },
+  {
+    prompt: "girl,motorcycles,biker outfits,helmet",
+    negative:
+      "(bad quality,low quality :1.6), multiple body part,incomplete body part,merged body part,deformed,blurry,bad anatomy,disfigured,poorly drawn face,mutation,extra_limb,ugly,poorly drawn hands,two heads,gross,disgusting,horrible",
+  },
+  {
+    prompt:
+      "girl,japan street,hoodie,street outfit,raining,neon in background,photograph,By Toei Animation,masterpiece,high quality",
+    negative:
+      "(bad quality,low quality :1.6), multiple body part,incomplete body part,merged body part,deformed,blurry,bad anatomy,disfigured,poorly drawn face,mutation,extra_limb,ugly,poorly drawn hands,two heads,gross,disgusting,horrible",
+  },
+  {
+    prompt:
+      "cat girl, paw gloves, long tail, animal_ears, fluffy, (big pants), fluffy sweater, sleeves, long sleeves, socks, slippers, ((((iridescent)))), ((((pearlescent)))), ((pastel colors)), (soft colors), (pink, blue, yellow, purple), cozy clutter, symbol-shaped pupils, dreamy, stars, bubbles, glitter, twinkles, light particles, light refraction, reflections, ribbons, silk, silk ribbon collar, cute stickers, pastel bandaids, stuffed animals, wallpaper, wearing headphones,neon lighting",
+    negative:
+      "(bad quality,low quality :1.6), multiple body part,incomplete body part,merged body part,deformed,blurry,bad anatomy,disfigured,poorly drawn face,mutation,extra_limb,ugly,poorly drawn hands,two heads,gross,disgusting,horrible",
+  },
+  {
+    prompt:
+      "1girl, police officer, crop top SWAT suit, long black hair, night, mask",
+    negative:
+      "(bad quality,low quality :1.6), multiple body part,incomplete body part,merged body part,deformed,blurry,bad anatomy,disfigured,poorly drawn face,mutation,extra_limb,ugly,poorly drawn hands,two heads,gross,disgusting,horrible",
+  },
+  {
+    prompt:
+      "girl,blue hair,bun hairstyle,chinese with dragon image,masterpiece,high quality,looking at camera",
+    negative:
+      "(bad quality,low quality :1.6), multiple body part,incomplete body part,merged body part,deformed,blurry,bad anatomy,disfigured,poorly drawn face,mutation,extra_limb,ugly,poorly drawn hands,two heads,gross,disgusting,horrible",
+  },
+  {
+    prompt: "school uniform,light blue hair,day",
+    negative:
+      "(bad quality,low quality :1.6), multiple body part,incomplete body part,merged body part,deformed,blurry,bad anatomy,disfigured,poorly drawn face,mutation,extra_limb,ugly,poorly drawn hands,two heads,gross,disgusting,horrible",
+  },
+  {
+    prompt:
+      "facing viewer,(1girl:1.3),(lying  pose:1.2),small breasts,long hair,(cyan hair,hair spread out:1.1),bangs,(school uniform:1.1),bowtie,hair flower,(white thighhighs:0.5),bed",
+    negative:
+      "(bad quality,low quality :1.6), multiple body part,incomplete body part,merged body part,deformed,blurry,bad anatomy,disfigured,poorly drawn face,mutation,extra_limb,ugly,poorly drawn hands,two heads,gross,disgusting,horrible",
+  },
+];
